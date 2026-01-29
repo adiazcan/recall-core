a4ea80ca0fe5b65e012bfa24f1d852691d40721a
Alberto Diaz Martin <adiazcan@hotmail.com>
2026-01-29 18:23:09 +0100

    fix: remove jsonencode from azapi_resource body for 2.0 compatibility

--- infra/modules/documentdb/main.tf
+++ infra/modules/documentdb/main.tf
@@ -22,7 +22,7 @@ resource "azapi_resource" "mongo_cluster" {
   parent_id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${var.resource_group_name}"
   tags      = var.tags
 
-  body = jsonencode({
+  body = {
     properties = {
       administrator = {
         userName = var.administrator_login
@@ -47,7 +47,7 @@ resource "azapi_resource" "mongo_cluster" {
         }
       ]
     }
-  })
+  }
 }
 
 locals {
